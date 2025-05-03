4e4051080f3404f24e396b24e06ce51c9c682e96
unknown <lounisbousbaine@gmail.com>
2025-05-03 19:03:31 +0200

    all

--- src/app/api/blog/[slug]/route.ts
+++ src/app/api/blog/[slug]/route.ts
@@ -2,26 +2,39 @@ import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/mongo";
 import BlogPost from "@/app/models/blog";
 
-export async function PUT(req: NextRequest, context: { params: { slug: string } }) {
+type ParamsType = {
+  params: {
+    slug: string;
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: ParamsType
+) {
   try {
     await dbConnect();
-    const slug = context.params.slug;
+    const { slug } = params;
     if (!slug) {
       return NextResponse.json({ error: "Slug parameter is required" }, { status: 400 });
     }
+
     const contentType = req.headers.get("content-type") || "";
     if (!contentType.includes("application/json")) {
       return NextResponse.json({ error: "Unsupported Content-Type" }, { status: 415 });
     }
+
     const body = await req.json();
     const updatedPost = await BlogPost.findOneAndUpdate(
       { slug },
       body,
       { new: true, runValidators: true }
     );
+
     if (!updatedPost) {
       return NextResponse.json({ error: "Blog post not found" }, { status: 404 });
     }
+
     return NextResponse.json(updatedPost);
   } catch (error: any) {
     return NextResponse.json({ error: error.message || "Failed to update blog post" }, { status: 500 });
