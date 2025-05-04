893d63e7d15b8bfe3acabab8ce1925bdaab552d4
unknown <lounisbousbaine@gmail.com>
2025-05-04 15:01:37 +0200

    all

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* other config options here */
   images: {
-    domains: ['localhost', 'maevashop.netlify.app'], // Added your Netlify domain
+    domains: ['localhost', 'maeva-tawny.vercel.app'], // Added your Netlify domain
   },
   // Add any other configuration options here
 };
