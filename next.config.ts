30f74a17b819350b9d6d4170307993f102a6d13c
unknown <lounisbousbaine@gmail.com>
2025-05-06 09:20:28 +0200

    all

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+
     domains: [
       'localhost',
-      'via.placeholder.com',
       'maevashop.netlify.app',
       'maeva-tawny.vercel.app'
     ],
+    // You can also use unoptimized for development
+    unoptimized: process.env.NODE_ENV === 'development',
+    // Use the default loader for local development
+    loader: process.env.NODE_ENV === 'development' ? 'default' : 'imgix',
+    // Use the default path for local development
+    
     // You can also use remotePatterns for more specific control
     remotePatterns: [
       {
