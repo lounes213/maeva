d68e533887ab9e461c132661260d8bcc81023298
unknown <lounisbousbaine@gmail.com>
2025-05-10 15:45:01 +0200

    all

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
 
-  async rewrites() {
-    return [
-      {
-        source: '/.well-known/appspecific/com.chrome.devtools.json',
-        destination: '/api/chrome-devtools' // or any endpoint you want
-      }
-    ]
-  },
+const nextConfig: NextConfig = {
+  /* other config options here */
   images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [16, 32, 48, 64, 96],
-
-    domains: [
-      'localhost',
-     
-      'maeva-three.vercel.app',
-      // Add your cloud storage domain here
-      'your-cloud-storage-domain.com'
-    ],
-  
-
-    // You can also use remotePatterns for more specific control
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-      }
-    ],
-    // Add this to ensure local images work
-    unoptimized: true
+    domains: ['localhost', 'maevashop.netlify.app'], // Added your Netlify domain
   },
   // Add any other configuration options here
 };
