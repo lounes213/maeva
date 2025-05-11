f3bd34b83cdfb86c8e5d087ece7ed88a79864f6d
unknown <lounisbousbaine@gmail.com>
2025-05-11 23:45:49 +0200

    all

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -115,7 +115,7 @@ const ProductsPage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 mt-23">
     <DashboardHeader user={user}/>
       {error && (
         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
