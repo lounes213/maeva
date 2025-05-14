6e32cfa6d54330be722115f08c239027153ec12e
unknown <lounisbousbaine@gmail.com>
2025-05-14 09:38:56 +0200

    Remove conflicting files and move search page to route group

--- src/app/search/page.config.js
+++
@@ -1,2 +0,0 @@
-// This file is deprecated and will be removed
-// Configuration has been moved to config.js
--- src/app/search/route.ts
+++
@@ -1,2 +0,0 @@
-// This file is intentionally left empty and will be removed
-// The functionality has been moved to /api/search/route.ts
